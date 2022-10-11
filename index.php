a28a20c6a9df14370be3596de50a9395b237601c
Emmy2721 <96807367+Emmy2721@users.noreply.github.com>
2022-10-11 11:28:01 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portal Page!</title>
+    <link href="css/styles.css" type="text/css"
+    rel="stylesheet">
+</head>
+<body>
+<h1><a href="index.php">Mahlet's Portal Page</a></h1>
+
+<div id="wrapper">
+<nav>
+
+<ul>
+
+<li><a href="">Switch</a></li>
+<li><a href="">Troubleshoot</a></li>
+<li><a href="">Calculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="">Gallery</a></li>
+</ul>
+
+
+</nav>
+
+<main>
+<h2>About Mahlet</h2>
+<img src="images/IMG-0023.jpg" alt="Mahlet">
+<p>My name is Mahlet Birhanu, I am originally from Ethiopia and currently 
+    attending database administration at Seattle central college, 
+    I plan to continue my degree in Web development which is why I am taking this class. 
+    I hope it will be fun. My goal is to finish my degree and continue 
+    my MA in the tech field and become successful, 
+    I like Hiking and singing and also spending time with my family.</p>
+
+
+<h2>MAMP</h2>
+ <img src="images/IMG-0020.jpg" alt="MAMP">
+
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ol>
+
+<li><a href="var.php">var.php</a></li>
+<li><a href="var2.php"></a></li>
+<li><a href="currency-logic.php">var.php</a></li>
+<li><a href="currency.php">var.php</a></li>
+<li><a href="heredoc.php">var.php</a></li>
+
+</ol>
+
+
+
+<h3>week 3</h3>
+<ol>
+
+<li><a href="if.php">var.php</a></li>
+<li><a href="foreach.php">var.php</a></li>
+<li><a href="foloop.php">var.php</a></li>
+<li><a href="date.php">var.php</a></li>
+<li><a href="switch.php">var.php</a></li>
+
+</ol>
+
+<h3>week 4</h3>
+<ol>
+
+<li><a href="form1.php">var.php</a></li>
+<li><a href="form2.php">var.php</a></li>
+<li><a href="form3.php">var.php</a></li>
+<li><a href="form-arithmetic.php">var.php</a></li>
+<li><a href="celcius.php">var.php</a></li>
+
+</ol>
+
+
+<h3>week 5</h3>
+<ol>
+
+<li><a href="currency1.php">var.php</a></li>
+<li><a href="currency2.php">var.php</a></li>
+<li><a href="currency3.php">var.php</a></li>
+<li><a href="currency4.php">var.php</a></li>
+<li><a href="null.php">var.php</a></li>
+
+</ol>
+
+<h3>week6</h3>
+<ol>
+
+<li><a href="form.php">var.php</a></li>
+<li><a href="implode.php">var.php</a></li>
+<li><a href="functions.php">var.php</a></li>
+
+</ol>
+
+
+
+<h3>week 7</h3>
+<ol>
+
+<li><a href="random.php">var.php</a></li>
+<li><a href="pictures.php">var.php</a></li>
+<li><a href="strings.php">var.php</a></li>
+
+
+</ol>
+
+<h3>week 8</h3>
+<ol>
+
+<li><a href="people.php">var.php</a></li>
+
+
+</ol>
+
+</aside>
+
+</div>
+
+
+
+
+
+</body>
+</html>
