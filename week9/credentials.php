bcc075c7586cec618e6b3218b371d095444005f8
emmy27 <96811283+Emmy2721@users.noreply.github.com>
2022-12-13 18:04:01 -0800

    Add files via upload

---
+++ week9/credentials.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//mbirhcom_school_projects - name of my database
+//mbirhcom_school_projects_user--- name of user
+
+
+define('DB_NAME', 'mbirhcom_user');
+define('DB_USER', 'mbirhcom_user_Final');
+define('DB_PASSWORD', 'Hanna1996@');
+define('DB_HOST', 'localhost');
+
